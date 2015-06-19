ee65f22c758f9b3e27e9db5f67d5c1eb8863f2de
xiaeryu <xiaeryu@nus.edu.sg>
2015-06-19 14:02:16 +0800

    Create ResFinder.sh

---
+++ ResFinder.sh
@@ -0,0 +1,14 @@
+## The ResFinder database is readily avialable at:
+## https://cge.cbs.dtu.dk/services/data.php
+
+## Step 1. Construct the database (Done only once):
+makeblastdb -in all_databases.fsa -out resfinder -dbtype nucl
+
+## Step 2. Blast the contigs in the database:
+blastn -query contigs.fa -db resfinder -task blastn -outfmt 7 > contigs.resfinder.blast.out
+
+## Step 3. Parse the blast output file:
+perl hitDB.pl all_databases.fsa contigs.resfinder.blast.out 0.95(can change) 80(canchange) > contigs.resfinder.parse.out
+
+## Step 4. Collapse the .parse.out file to collapse overlapping hits.
+perl collapsePS.pl contigs.resfinder.parse.out > contigs.resfinder.filter.out
