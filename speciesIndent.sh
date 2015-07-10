13ec373029520e7eb4a49a9b2a6bd413b4100585
xiaeryu <xiaeryu@nus.edu.sg>
2015-07-10 14:22:27 +0800

    Create speciesIndent.sh

---
+++ speciesIndent.sh
@@ -0,0 +1,6 @@
+input=$1
+database=$2
+
+makeblastdb -in $database -out $database -dbtype nucl
+blastn -db $database -query $input -task blastn -outfmt 7 > $input\_blast.out
+grep -v '#' $input\_blast.out | sort -nrk12 | head -3 | awk '{print $2}' | while read line; do grep $line $database; done > $input.species
