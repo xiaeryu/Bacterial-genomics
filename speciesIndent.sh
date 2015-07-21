69d24299ec49d898df6b8594dc13dd2a69aff2b7
xiaeryu <xiaeryu@nus.edu.sg>
2015-07-21 09:44:00 +0800

    Update speciesIndent.sh

--- speciesIndent.sh
+++ speciesIndent.sh
@@ -3,4 +3,4 @@ database=$2
 
 makeblastdb -in $database -out $database -dbtype nucl
 blastn -db $database -query $input -task blastn -outfmt 7 > $input\_sp.blast.out
-grep -v '#' $input\_sp.blast.out | sort -nrk12 | head -3 | awk '{print $2}' | while read line; do grep $line $database; done > $input.species
+grep -v '#' $input\_sp.blast.out | awk '{if($4 > 700) print}' | sort -nrk12 | head -3 | awk '{print $2}' | while read line; do grep $line $database; done > $input.species
