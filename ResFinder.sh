70180408ea99fa2bdb01244f9bb07aee1054dbf6
xiaeryu <xiaeryu@nus.edu.sg>
2015-10-02 16:24:32 +0800

    Update ResFinder.sh

--- ResFinder.sh
+++ ResFinder.sh
@@ -8,7 +8,7 @@ makeblastdb -in all_databases.fsa -out resfinder -dbtype nucl
 blastn -query contigs.fa -db resfinder -task blastn -outfmt 7 > contigs.resfinder.blast.out
 
 ## Step 3. Parse the blast output file:
-perl hitDB.pl all_databases.fsa contigs.resfinder.blast.out 0.95(can change) 80(canchange) > contigs.resfinder.parse.out
+perl hitDB.pl all_databases.fsa contigs.resfinder.blast.out 0.95(can change) 80(can change) > contigs.resfinder.parse.out
 
 ## Step 4. Collapse the .parse.out file to collapse overlapping hits.
 sort -k1 -nk7 contigs.resfinder.parse.out | perl collapsePS.pl  - > contigs.resfinder.filter.out
